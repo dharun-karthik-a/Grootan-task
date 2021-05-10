fcdd173dd682bfb01fb83e86857302743274813e
Dharun Karthik A <51316249+dharun-karthik@users.noreply.github.com>
2021-05-10 09:48:33 +0530

    Changed to API

--- Display/views.py
+++ Display/views.py
@@ -24,9 +24,9 @@ def home(request):
 
 def result(request):
     if request.user.is_authenticated:
-        # response = requests.get("https://hello.free.beeceptor.com/task")
-        # data = response.json()
-        data=[{'name': 'Dharun', 'age': 10, 'dob': '20-10-2020', 'firstName': 'Dharun', 'lastName': 'Karthik', 'more': {'address_line1': 'Adress  Line 1 will be displayed here', 'address_line2': 'Adress  Line 2 will be displayed here', 'address_line3': 'Adress  Line 3 will be displayed here', 'phone': '+91-9943962784'}}, {'name': 'Darshan', 'age': 10, 'dob': '20-10-2020', 'firstName': 'Darshan', 'lastName': 'M R', 'more': {'address_line1': 'Adress  Line 1 will be displayed here', 'address_line2': 'Adress  Line 2 will be displayed here', 'address_line3': 'Adress  Line 3 will be displayed here', 'phone': '+91-908086992'}}]
+        response = requests.get("https://hello.free.beeceptor.com/task")
+        data = response.json()
+        #data=[{'name': 'Dharun', 'age': 10, 'dob': '20-10-2020', 'firstName': 'Dharun', 'lastName': 'Karthik', 'more': {'address_line1': 'Adress  Line 1 will be displayed here', 'address_line2': 'Adress  Line 2 will be displayed here', 'address_line3': 'Adress  Line 3 will be displayed here', 'phone': '+91-9943962784'}}, {'name': 'Darshan', 'age': 10, 'dob': '20-10-2020', 'firstName': 'Darshan', 'lastName': 'M R', 'more': {'address_line1': 'Adress  Line 1 will be displayed here', 'address_line2': 'Adress  Line 2 will be displayed here', 'address_line3': 'Adress  Line 3 will be displayed here', 'phone': '+91-908086992'}}]
         json_list = json.dumps(data)
         nameAge=zip(tolist(data,"name"),tolist(data, "age"))
         return render(request,"result.html",{
